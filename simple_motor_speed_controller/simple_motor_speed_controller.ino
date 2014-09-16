99630e4e823620bc9485e4e4a257218e2b99cf4a
Peter Wallhead <peter.wallhead@gmail.com>
2014-09-17 00:14:03 +1000

    Added motor cutoff value to stop the motor stuttering at low speeds

--- simple_motor_speed_controller/simple_motor_speed_controller.ino
+++ simple_motor_speed_controller/simple_motor_speed_controller.ino
@@ -2,16 +2,22 @@ int potPin = A0;
 int motorPin = 9;
 int potValue = 0;
 int motorValue = 0;
+int motorCutoff = 228;
+
 void setup() {
  Serial.begin(38400);
 }
 void loop() {
  potValue = analogRead(potPin);  
  motorValue = map(potValue, 0, 1023, 0, 255);
- analogWrite(motorPin, motorValue);  
+ if(motorValue > motorCutoff) {
+   analogWrite(motorPin, motorValue);
+ } else {
+   digitalWrite(motorPin, LOW);
+ } 
  Serial.print("potentiometer = " );     
  Serial.print(potValue);
  Serial.print("\t motor = ");
  Serial.println(motorValue);
- delay(100);    
+ delay(2);    
 }
